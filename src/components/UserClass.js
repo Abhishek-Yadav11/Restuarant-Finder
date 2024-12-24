22bb09cb240fe0438d65ec7b5d405749201b772e
Abhishek-Yadav11 <abhishek1102yadav@gmail.com>
2024-12-24 12:56:56 +0530

    L8

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,13 +4,31 @@ class UserClass extends React.Component {
      
     constructor(props) {
         super(props);
+        this.state = {
+            userinfo: {
+                name: "Default Name",
+                location: "India",
+            }
+        };
     }
 
+    async componentDidMount() {
+        //API call
+        const data = await fetch("https://api.github.com/users/Abhishek-Yadav11");
+        const json = await data.json();
+
+        console.log(json);
+        this.setState({
+            userinfo: {
+                name: json.name,
+                location: json.location,
+            }
+        }); 
+    }
 
     render(){
 
-        const {name,location}=this.props;
-
+        const {name, location} = this.state.userinfo;
         return (
             <div className="user-card">
                 <h3>Name: {name}</h3>
